bf540a78c0f8576a471a2a970413cfc45994dafd
Thiago <thiago.silvamarcelo56@gmail.com>
2022-12-19 20:29:09 -0300

    [DEP] add dependencies

---
+++ src/test/java/com/newgo/mercadoapi/MercadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newgo.mercadoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
