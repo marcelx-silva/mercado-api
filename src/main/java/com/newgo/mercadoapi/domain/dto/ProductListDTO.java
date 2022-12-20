45f4afdf270b13defc32eb0fb45fe0c66d46e949
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-20 10:40:34 -0300

    add Product DTO to show products from a shopping list

---
+++ src/main/java/com/newgo/mercadoapi/domain/dto/ProductListDTO.java
@@ -0,0 +1,16 @@
+package com.newgo.mercadoapi.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductListDTO {
+    private String name;
+    private String description;
+    private Integer quantity;
+}
