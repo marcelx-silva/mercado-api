8c4119b499eed5f07a9b02d9657342a8cbc3676c
Thiago <thiago.silvamarcelo56@gmail.com>
2022-12-19 21:24:24 -0300

    add service to store products at h2 database

---
+++ src/main/java/com/newgo/mercadoapi/service/product/ProductServiceH2.java
@@ -0,0 +1,77 @@
+package com.newgo.mercadoapi.service.product;
+
+import com.newgo.mercadoapi.domain.dto.ProductDTO;
+import com.newgo.mercadoapi.domain.model.Product;
+import com.newgo.mercadoapi.repository.ProductRepository;
+import com.newgo.mercadoapi.service.imageproduct.ImageProductService;
+import com.newgo.mercadoapi.service.product.ImageProductService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class ProductServiceH2 implements ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    ImageProductService imageProductService;
+
+
+    @Override
+    @Transactional
+    public void save(ProductDTO productDTO) {
+        Product product = modelMapper.map(productDTO, Product.class);
+        product.setImageProduct(imageProductService.findByURL(productDTO.getImageProductURL()));
+        productRepository.save(product);
+    }
+
+    @Override
+    public Set<ProductDTO> findAll() {
+        Set<ProductDTO> productDTOSet = new HashSet<>();
+        productRepository.findAll().
+                forEach(product -> productDTOSet.
+                        add(modelMapper.map(product,ProductDTO.class)));
+        return productDTOSet;
+    }
+
+    @Override
+    public Optional<ProductDTO> findById(UUID uuid) {
+        return Optional.ofNullable(modelMapper.map(productRepository.findById(uuid), ProductDTO.class));
+    }
+
+    @Override
+    public Optional<ProductDTO> findByName(String name) {
+        return Optional.ofNullable(modelMapper.map(productRepository.findProductByName(name), ProductDTO.class));
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(UUID uuid) {
+        productRepository.deleteById(uuid);
+    }
+
+    @Override
+    @Transactional
+    public void deleteByName(String name) {
+        productRepository.deleteProductByName(name);
+    }
+
+    @Override
+    public Set<ProductDTO> findAllByAtivadoIsTrue() {
+        Set<ProductDTO> productDTOSet = new HashSet<>();
+        productRepository.findAllByStatusIsTrue().
+                forEach(product -> productDTOSet.
+                        add(modelMapper.map(product,ProductDTO.class)));
+        return productDTOSet;
+    }
+}
