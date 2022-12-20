9a169073dabac956578fb0fabd783b51d6718320
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-20 11:18:58 -0300

    add DTO to response login authentication

---
+++ src/main/java/com/newgo/mercadoapi/domain/dto/AuthResponseDTO.java
@@ -0,0 +1,13 @@
+package com.newgo.mercadoapi.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String accessToken;
+}
+
