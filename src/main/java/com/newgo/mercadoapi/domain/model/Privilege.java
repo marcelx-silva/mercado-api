b7fe3790e495a9a638d62ad46635a93b7c5fcbd2
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-19 22:26:45 -0300

    add Privilege model

---
+++ src/main/java/com/newgo/mercadoapi/domain/model/Privilege.java
@@ -0,0 +1,28 @@
+package com.newgo.mercadoapi.domain.model;
+
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "privileges")
+public class Privilege implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+    @Column(name = "name",unique = true,nullable = false)
+    private String name;
+
+    public Privilege(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
+}
