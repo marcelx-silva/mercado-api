40780185a6495874c6c9fe1cbb7a66667e57637a
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-22 12:09:13 -0300

    add method to generate secret key

--- src/main/java/com/newgo/mercadoapi/config/security/utils/JwtTokenUtil.java
+++ src/main/java/com/newgo/mercadoapi/config/security/utils/JwtTokenUtil.java
@@ -2,28 +2,36 @@ package com.newgo.mercadoapi.config.security.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Objects;
 
 @Component
 public class JwtTokenUtil {
-    private static final Integer EXPIRE_DURATION = 24*60*60*1000;
+    private static final Integer EXPIRE_DURATION = 24 * 60 * 60 * 1000;
 
     @Value("${app.jwt.secret}")
     private String SECRET_KEY;
-    public String generateAccessToken(String username){
+
+    public String generateAccessToken(String username) {
+        SecretKey secretKey = getSecretKey();
         return Jwts.builder()
                 .setSubject(username)
-                .setExpiration(new Date(System.currentTimeMillis()+EXPIRE_DURATION))
-                .signWith(SignatureAlgorithm.ES256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
+                .signWith(secretKey)
                 .compact();
     }
 
-    public boolean isTokenExists(String token){
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public boolean isTokenExists(String token) {
         return Objects.nonNull(token);
     }
 
@@ -31,12 +39,14 @@ public class JwtTokenUtil {
         return token.startsWith("Bearer");
     }
 
-    public String getUsername(String token){
+    public String getUsername(String token) {
         return getClaims(token).getSubject();
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJwt(token).getBody();
+        SecretKey secretKey = getSecretKey();
+        return Jwts.parserBuilder().setSigningKey(secretKey)
+                .build().parseClaimsJws(token).getBody();
     }
 
 }
