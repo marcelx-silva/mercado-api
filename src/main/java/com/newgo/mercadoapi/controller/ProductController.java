771bdf42b32ce6954d61096210add178b85ea109
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-23 04:20:36 -0300

    add endpoint to add product with multipart file

--- src/main/java/com/newgo/mercadoapi/controller/ProductController.java
+++ src/main/java/com/newgo/mercadoapi/controller/ProductController.java
@@ -1,13 +1,21 @@
 package com.newgo.mercadoapi.controller;
 
+import com.newgo.mercadoapi.domain.dto.ImageProductDTO;
 import com.newgo.mercadoapi.domain.dto.ProductDTO;
+import com.newgo.mercadoapi.domain.model.ImageProduct;
+import com.newgo.mercadoapi.domain.model.Product;
+import com.newgo.mercadoapi.service.imageproduct.ImageProductService;
+import com.newgo.mercadoapi.service.imageproduct.Storage;
 import com.newgo.mercadoapi.service.product.ProductService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,12 +26,45 @@ import java.util.UUID;
 public class ProductController {
     @Autowired
     ProductService productService;
+    @Autowired
+    Storage storage;
+    @Autowired
+    ImageProductService imageProductService;
+
 
     @PostMapping("/adm/save")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Object> save(@RequestBody ProductDTO productDTO) {
         productService.save(productDTO);
         return ResponseEntity.ok().body(productDTO);
+    }
+
+    @PostMapping("/adm/save/product")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Object> saveWithImage(@RequestPart("img") MultipartFile multipartFile,
+                                        @RequestParam String descImg,
+                                        @RequestParam Boolean statusProd,
+                                        @RequestParam String nameProd,
+                                        @RequestParam String descProd,
+                                        @RequestParam int quantProd) {
+
+        storage.saveImage(multipartFile);
+        ImageProductDTO imageProductDTO =
+                new ImageProductDTO(multipartFile.getOriginalFilename().toLowerCase(),
+                        storage.getImagePath(), descImg);
+        imageProductService.save(imageProductDTO);
+
+
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setName(nameProd);
+        productDTO.setDescription(descProd);
+        productDTO.setStatus(statusProd);
+        productDTO.setQuantity(quantProd);
+        productDTO.setImageProductURL(storage.getImagePath());
+
+
+        productService.save(productDTO);
+        return ResponseEntity.ok().body(null);
     }
 
     @GetMapping("/adm/all")
@@ -91,7 +132,7 @@ public class ProductController {
         if (product.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product does not exist");
 
-        productService.updateProduct(uuid,productDTO);
+        productService.updateProduct(uuid, productDTO);
         return ResponseEntity.ok().body(productService.findById(uuid));
     }
 
