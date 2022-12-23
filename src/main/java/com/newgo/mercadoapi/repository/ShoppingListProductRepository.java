a8ce000d1e050b52e1025fceeadbd5c7f7732ffb
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-23 01:21:42 -0300

    add query method to remove product from a list

--- src/main/java/com/newgo/mercadoapi/repository/ShoppingListProductRepository.java
+++ src/main/java/com/newgo/mercadoapi/repository/ShoppingListProductRepository.java
@@ -1,12 +1,27 @@
 package com.newgo.mercadoapi.repository;
 
+import com.newgo.mercadoapi.domain.model.Product;
 import com.newgo.mercadoapi.domain.model.ShoppingList;
 import com.newgo.mercadoapi.domain.model.ShoppingListProduct;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ShoppingListProductRepository extends CrudRepository<ShoppingListProduct, UUID> {
     List<ShoppingListProduct> findAllByShoppingList(ShoppingList shoppingList);
+
+    @Modifying
+    @Query(value =  "DELETE " +
+            "ShoppingListProduct pl " +
+            "WHERE " +
+            "pl.products.uuid =:product AND " +
+            "pl.shoppingList.uuid =:list")
+    void removeProductFromList(@Param("product") UUID productId, @Param("list") UUID productListId);
+
 }
