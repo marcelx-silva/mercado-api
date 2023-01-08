45de8d452af1a8f042edb74f04798475cb8103ab
Thiago <thiago.silvamarcelo56@gmail.com>
2023-01-08 17:00:18 -0300

    add category model

---
+++ src/main/java/com/newgo/mercadoapi/domain/model/Category.java
@@ -0,0 +1,23 @@
+package com.newgo.mercadoapi.domain.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "description",nullable = true)
+    private String description;
+}
